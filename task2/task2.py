6f10e684bbcd9d897b92bcd6585ae01df2549dd9
yar4e <yarche.off@gmail.com>
2025-07-02 15:06:27 +0300

    Task2 complete
    Check instruction in readme

---
+++ task2/task2.py
@@ -0,0 +1,39 @@
+import sys
+import math
+
+# Проверка на наличие двух файлов
+if len(sys.argv) != 3:
+    print("Использовать таким образом: python3 script.py circle.txt points.txt")
+    sys.exit(1)
+
+# Открытие первого файла и считывание центра и радиуса
+file = open(sys.argv[1], "r")
+line1 = file.readline().strip()
+x_c, y_c = line1.split()
+x_c = float(x_c)
+y_c = float(y_c)
+r = float(file.readline().strip())
+file.close()
+
+# Открытие второго файла и получение точек
+file = open(sys.argv[2], "r")
+points = []
+for line in file:
+    line = line.strip()
+    if line != "":
+        x, y = line.split()
+        points.append( (float(x), float(y)) )
+file.close()
+
+# Проверка каждой точки
+for p in points:
+    x = p[0]
+    y = p[1]
+    dist = math.sqrt((x - x_c)**2 + (y - y_c)**2)
+# Проверяем где точка находится
+    if abs(dist - r) < 0.000000000001:
+        print(0)
+    elif dist < r:
+        print(1)
+    else:
+        print(2)
